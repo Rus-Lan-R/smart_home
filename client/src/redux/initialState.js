9ef304b272afea5e8135ec832f9dbb7e2ce7dafc
ruslan <ruslan.rodionow98@gmail.com>
2021-07-18 15:36:00 +0300

    save

---
+++ client/src/redux/initialState.js
@@ -0,0 +1,26 @@
+const initState = {
+  user: null,
+  scanningIP: [],  
+  loader: false,
+  rooms: { 
+    items: [],
+    isLoading: false,
+    error: null,
+  },
+  devices: {
+    items: [],
+    isLoading: false,
+    error: null,
+  },
+  scenarios: {
+    items: [],
+    isLoading: false,
+    error: null,
+  }
+};
+
+const getInitState = () => {
+  const stateFromLS = JSON.parse(window.localStorage.getItem("redux"));
+  return stateFromLS ? stateFromLS : initState;
+};
+export default getInitState;
