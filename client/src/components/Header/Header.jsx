9db49bfab0ecfac75a5b2a2b857d9715830a7422
Oleg <koruzozen@gmail.com>
2021-07-19 21:48:42 +0300

    header fix

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,15 +1,13 @@
 import React from "react";
-import { alpha, makeStyles } from "@material-ui/core/styles";
+import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
-import InputBase from "@material-ui/core/InputBase";
 import Badge from "@material-ui/core/Badge";
 import MenuItem from "@material-ui/core/MenuItem";
 import Menu from "@material-ui/core/Menu";
 import MenuIcon from "@material-ui/icons/Menu";
-import SearchIcon from "@material-ui/icons/Search";
 import AccountCircle from "@material-ui/icons/AccountCircle";
 import MailIcon from "@material-ui/icons/Mail";
 import NotificationsIcon from "@material-ui/icons/Notifications";
@@ -30,43 +28,6 @@ const useStyles = makeStyles((theme) => ({
 			display: "block",
 		},
 	},
-	// search: {
-	// 	position: "relative",
-	// 	borderRadius: theme.shape.borderRadius,
-	// 	backgroundColor: alpha(theme.palette.common.white, 0.15),
-	// 	"&:hover": {
-	// 		backgroundColor: alpha(theme.palette.common.white, 0.25),
-	// 	},
-	// 	marginRight: theme.spacing(2),
-	// 	marginLeft: 0,
-	// 	width: "100%",
-	// 	[theme.breakpoints.up("sm")]: {
-	// 		marginLeft: theme.spacing(3),
-	// 		width: "auto",
-	// 	},
-	// },
-	// searchIcon: {
-	// 	padding: theme.spacing(0, 2),
-	// 	height: "100%",
-	// 	position: "absolute",
-	// 	pointerEvents: "none",
-	// 	display: "flex",
-	// 	alignItems: "center",
-	// 	justifyContent: "center",
-	// },
-	// inputRoot: {
-	// 	color: "inherit",
-	// },
-	// inputInput: {
-	// 	padding: theme.spacing(1, 1, 1, 0),
-	// 	// vertical padding + font size from searchIcon
-	// 	paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-	// 	transition: theme.transitions.create("width"),
-	// 	width: "100%",
-	// 	[theme.breakpoints.up("md")]: {
-	// 		width: "20ch",
-	// 	},
-	// },
 	sectionDesktop: {
 		display: "none",
 		[theme.breakpoints.up("md")]: {
@@ -200,19 +161,6 @@ export default function Header() {
 					<Typography className={classes.title} variant="h6" noWrap>
 						{userName ? userName : "Smart Home"}
 					</Typography>
-					{/* <div className={classes.search}>
-						<div className={classes.searchIcon}>
-							<SearchIcon />
-						</div>
-						<InputBase
-							placeholder="Search…"
-							classes={{
-								root: classes.inputRoot,
-								input: classes.inputInput,
-							}}
-							inputProps={{ "aria-label": "search" }}
-						/>
-					</div> */}
 					<div className={classes.grow} />
 					<div className={classes.sectionDesktop}>
 						{userName ? (
@@ -222,15 +170,6 @@ export default function Header() {
 								<NotificationsIcon />
 							</Badge>
 						</IconButton>
-						{/* <IconButton
-							edge="end"
-							aria-label="account of current user"
-							aria-controls={menuId}
-							aria-haspopup="true"
-							onClick={handleProfileMenuOpen}
-							color="inherit">
-							<AccountCircle />
-						</IconButton> */}
 								<Typography className={classes.title} variant="h6" noWrap>
 									<NavLink exact to="/auth/signout" className="nav-link" activeClassName="active">
 										Sign Out
@@ -254,14 +193,7 @@ export default function Header() {
 									</Typography>
 								</MenuItem>
 							</>
-						)}
-
-						{/* <IconButton aria-label="show 4 new mails" color="inherit">
-							<Badge badgeContent={4} color="secondary">
-								<MailIcon />
-							</Badge>
-						</IconButton> */}
-						
+						)}						
 					</div>
 					<div className={classes.sectionMobile}>
 						<IconButton
