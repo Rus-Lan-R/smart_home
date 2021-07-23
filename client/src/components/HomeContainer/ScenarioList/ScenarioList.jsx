01714517c4638d7060dc536d34a7e118a8e1e9cb
Oleg <koruzozen@gmail.com>
2021-07-23 15:00:53 +0300

    gif

--- client/src/components/HomeContainer/ScenarioList/ScenarioList.jsx
+++ client/src/components/HomeContainer/ScenarioList/ScenarioList.jsx
@@ -67,7 +67,7 @@ export default function ScenarioList() {
     {
       name: "Relax",
       status: false,
-      picture: "https://images.unsplash.com/photo-1550223640-23097fc71cb2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+      picture: "5.gif",
     },
     {
       name: "BoomBoom",
